dca538b87fa38a7517b4d7db342c7bd943eaf6ff
Maincrepa <maincrep2020@gmail.com>
2026-03-18 09:31:01 +0100

    Creat admin amb métodes: banearUsuari, eliminarUsuari, LlistarUsuaris + tostring

--- RA2-4/SocialApp/socialapp/src/com/socialapp/model/Admin.java
+++ RA2-4/SocialApp/socialapp/src/com/socialapp/model/Admin.java
@@ -1,5 +1,7 @@
 package com.socialapp.model;
 
+import com.socialapp.controller.RedSocial;
+
 /*
 Admin hereta de Usuario amb extends. No cal redeclarar cap atribut ni metode de Usuario, ja
 els te per herencia. Afegeix un array de Strings anomenat baneats per guardar els usernames
@@ -13,6 +15,55 @@ red.eliminar().
 Sobreescriu toString() amb @Override per indicar que l'usuari es administrador
 */
 
-public class Admin {
+public class Admin extends Usuario {
+    private String[] baneats;
+    private static final int MAX_BANEATS = 50;
+
+    /*
+    ==========================================
+        CONSTRUCTOR
+    ==========================================
+    */
+    public Admin(String username, String contrasenya, String email) {
+        super(username, contrasenya, email);
+        this.baneats = new String[MAX_BANEATS];
+    }
+
+    /*
+    ==========================================
+        MÉTODES ADMIN
+    ==========================================
+    */
+
+    public void banearUsuari(RedSocial red, String username) {
+        if (this.baneats.length >= MAX_BANEATS) {
+            System.out.println("No es poden banear més usuaris.");
+            return;
+        }
+        this.baneats[this.baneats.length] = username; // Afegim el username a l'array de baneats
+    }
+
+    public void eliminarUsuari(RedSocial red, String username) {
+        red.eliminar(username); // Cridem al metode eliminar de RedSocial
+    }
+
+    public void llistarUsuaris(RedSocial red) {
+        String[][] usuaris = red.getUsuaris(); // Agafem la llista d'usuaris de RedSocial
+        
+        for (int i = 0; i < red.getNumUsuaris(); i++) {
+            System.out.println("Username: " + usuaris[i][0] + ", Email: " + usuaris[i][2]);
+        }
+    }
+
+    /*
+    ==========================================
+        MÉTODES TOSTRING
+    ==========================================
+    */
+
+    @Override
+    public String toString() {
+        return "Admin{username = '" + this.getUsername() + "', email = " + this.getEmail() + "', posts: " + this.getPosts().length + ", amics: " + this.getAmics().length + "}";
+    }
     
 }
