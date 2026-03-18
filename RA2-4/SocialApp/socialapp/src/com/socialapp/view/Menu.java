164c95d30fa3190002c8e535f68426d8cbb99ffb
Maincrepa <maincrep2020@gmail.com>
2026-03-18 10:24:32 +0100

    Created menu methods for user

--- RA2-4/SocialApp/socialapp/src/com/socialapp/view/Menu.java
+++ RA2-4/SocialApp/socialapp/src/com/socialapp/view/Menu.java
@@ -25,14 +25,28 @@ public class Menu {
     */
 
     public void mostrarMenuPrincipal(RedSocial redSocial) {
+        System.out.println("DEBUG: Mostrant menu principal.");
         boolean active = true;
+        Usuario usuari = null;
 
         while (active) {
-            int option = menuOpcions();
+            int option;
+            if (usuari != null) { // mostrar versió del menu per continuar
+                option = menuOpcions(0);
+            } else {  // mostrar versió del menu per fer login
+                option = menuOpcions(1);
+            }
+            
 
             switch (option) {
                 case 1: // LOGIN
-                    login(redSocial);
+                    System.out.println("DEBUG: LOGIN. | " + usuari);
+                    
+                    if (usuari != null) {
+                        mostrarMenuUsuari(usuari);
+                    } else {
+                        usuari = login(redSocial);
+                    }
                     break;
 
                 case 0: // SORTIR
@@ -54,7 +68,7 @@ public class Menu {
     ==========================================
     */
 
-    private static int menuOpcions() {
+    private static int menuOpcions(int option) {
         int escull = -1;
 
         String menuString = """
@@ -65,12 +79,12 @@ public class Menu {
 
         ==================================
 
-            1. Login
+            1. %s
             0. Salir    
 
         ==================================
 
-        """;
+        """.formatted(option == 1 ? "Login" : "Continuar");
 
         while (!(escull >= 0 && escull <= 1)) {
             System.out.println(menuString);
@@ -88,7 +102,32 @@ public class Menu {
     */
 
     public static void mostrarMenuUsuari(Usuario u) {
+        int escull = printMenuUsuari();
 
+        switch (escull) {
+            case 1: // VEURE PERFIL
+                veurePerfil(u);
+                break;
+
+            case 2: // AFEGIR AMIC
+                afegirAmic(u);
+                break;
+
+            case 3: // ELIMINAR AMIC
+                eliminarAmic(u);
+                break;
+
+            case 4: // CREAR POST
+                crearPost(u);
+                break;
+
+            case 5: // ELIMINAR POST
+                eliminarPost(u);
+                break;
+
+            default:
+                break;
+        }
     }
 
     public static void mostrarMenuAdmin(Admin a) {
@@ -97,11 +136,116 @@ public class Menu {
 
     /*
     ==========================================
+        MÉTODE PRINT MENU USUARI i ADMIN
+    ==========================================
+    */
+
+    private static int printMenuUsuari() {
+        int escull = -1;
+
+        System.out.println("""
+                
+        ==================================
+            MENU USUARI
+        ==================================
+        """);
+
+        String menuString = """
+
+            1. Veure perfil
+            2. Afegir amic
+            3. Eliminar amic
+            4. Crear post
+            5. Eliminar post
+            0. Sortir 
+
+        ==================================
+
+        """;
+
+        while (!(escull >= 0 && escull <= 5)) {
+            System.out.println(menuString);
+            System.out.print("Opció: ");
+            escull = input.nextInt();
+        }
+
+        return escull;
+    }
+
+    /*
+    ==========================================
+        MÉTODES USUARI
+    ==========================================
+    */
+
+    private static void veurePerfil(Usuario u) {
+        System.out.println(u.toString());
+    }
+
+    private static void afegirAmic(Usuario u) {
+        System.out.print("Introdueix el username de l'amic que vols afegir: ");
+        String username = input.next();
+
+        boolean resultat = u.afegirAmic(username);
+        if (resultat) {
+            System.out.println("Amic afegit correctament.");
+        } else {
+            System.out.println("No s'ha pogut afegir l'amic.");
+        }
+    }
+
+    private static void eliminarAmic(Usuario u) {
+        System.out.print("Introdueix el username de l'amic que vols eliminar: ");
+        String username = input.next();
+
+        boolean resultat = u.eliminarAmic(username);
+        if (resultat) {
+            System.out.println("Amic eliminat correctament.");
+        } else {
+            System.out.println("No s'ha pogut eliminar l'amic.");
+        }
+    }
+
+    private static void crearPost(Usuario u) {
+        System.out.print("Introdueix el text del post: ");
+        String text = input.next();
+
+        boolean resultat = u.publicarPost(text);
+        if (resultat) {
+            System.out.println("Post creat correctament.");
+        } else {
+            System.out.println("No s'ha pogut crear el post.");
+        }
+    }
+
+    private static void eliminarPost(Usuario u) {
+        System.out.print("Introdueix l'index del post que vols eliminar: ");
+        int index = input.nextInt();
+
+        boolean resultat = u.eliminarPost(index);
+        if (resultat) {
+            System.out.println("Post eliminat correctament.");
+        } else {
+            System.out.println("No s'ha pogut eliminar el post.");
+        }
+    }
+
+    /*
+    ==========================================
+        MÉTODES ADMIN
+    ==========================================
+    */
+
+
+
+
+    /*
+    ==========================================
         MÉTODE LOGIN
     ==========================================
     */
 
-    private void login(RedSocial redSocial) {
+    private Usuario login(RedSocial redSocial) {
         // Demana username i contrasenya, comprova que existeix l'usuari i que la contrasenya és correcta. Retorna l'objecte Usuario si es correcte, o null si no.
         System.out.println("""
 
@@ -120,7 +264,7 @@ public class Menu {
         Usuario usuari = redSocial.login(username, contrasenya);
         if (usuari == null) {
             System.out.println("No s'ha pogut fer login.");
-            return;
+            return null;
         }
 
         System.out.println("Login correcte. Benvingut " + usuari.getUsername() + "!");
@@ -130,5 +274,6 @@ public class Menu {
         } else {
             mostrarMenuUsuari(usuari);
         }
+        return usuari;
     }
 }
