c83ffc651ee6ff9a10f457370251cc0f51330d0a
Maincrepa <maincrep2020@gmail.com>
2026-04-08 09:18:26 +0200

    Finalitzem el canvi de Array a ArrayList

--- RA2-4/ExercicisVacances/EX2_Supermercat_ArrayList/src/Supermercat.java
+++ RA2-4/ExercicisVacances/EX2_Supermercat_ArrayList/src/Supermercat.java
@@ -1,9 +1,32 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Supermercat {
-    public static String[] nomsProductes = {"Llet (1L)", "Pa de motlle", "Pollastre (1kg)", "Arròs (1kg)", "Pomes (1kg)", "Aigua mineral (6L)", "Cereals", "Iogurt pack x4"};
-    public static double[] preusProductes = {1.20, 1.50, 5.99, 1.80, 2.50, 3.20, 3.75, 2.10};
-    public static int[] quantitatsCarret = new int[nomsProductes.length]; // "array 1D on l'índex i correspon al producte i del catàleg. Si quantitatsCarret[2] = 3, vol dir que hi ha 3 unitats del producte 2 al carret."
+    public static ArrayList<String> nomsCarret = new ArrayList<String>() {{
+        add("Llet (1L)");
+        add("Pa de motlle");
+        add("Pollastre (1kg)");
+        add("Arròs (1kg)");
+        add("Pomes (1kg)");
+        add("Aigua mineral (6L)");
+        add("Cereals");
+        add("Iogurt pack x4");
+    }};
+    public static ArrayList<Double> preusCarret = new ArrayList<Double>() {{
+        add(1.20);
+        add(1.50);
+        add(5.99);
+        add(1.80);
+        add(2.50);
+        add(3.20);
+        add(3.75);
+        add(2.10);
+    }};
+    public static ArrayList<Integer> quantitatsCarret = new ArrayList<Integer>() {{
+        for (int i = 0; i < nomsCarret.size(); i++) {
+            add(0);
+        }
+    }};
 
     static Scanner input = new Scanner(System.in);
 
@@ -62,8 +85,8 @@ public class Supermercat {
 
     public static void veureCataleg() {
         System.out.println("--- CATALEG DE PRODUCTES ---");
-        for (int i = 0; i < nomsProductes.length; i++) { // Mostrem tots els productes amb un bucle
-            System.out.printf("    %d. %s - %.2f EUR\n", i + 1, nomsProductes[i], preusProductes[i]);
+        for (int i = 0; i < nomsCarret.size(); i++) { // Mostrem tots els productes amb un bucle
+            System.out.printf("    %d. %s - %.2f EUR\n", i + 1, nomsCarret.get(i), preusCarret.get(i));
             // Print de l'ID del producte (int), nom del producte (string) i preu (double amb 2 decimals)
         }
     }
@@ -72,7 +95,7 @@ public class Supermercat {
         int producte_id = -1; // guardem l'ID del producte que vol afegir al carret
         int quantitat = -1; // guardem quants productes vol afegir al carret
 
-        while (!(producte_id >= 1 && producte_id <= nomsProductes.length)) {
+        while (!(producte_id >= 1 && producte_id <= nomsCarret.size())) {
             System.out.print("Introdueix l'ID del producte: ");
             producte_id = input.nextInt();
         }
@@ -82,9 +105,9 @@ public class Supermercat {
             quantitat = input.nextInt();
         }
 
-        quantitatsCarret[producte_id - 1] += quantitat; // Suma la quantitat al carret (recordem que l'ID del producte PEL USUARI és 1-based i l'índex de l'array és 0-based, per aixo fem producte_id - 1)
+        quantitatsCarret.set(producte_id - 1, quantitatsCarret.get(producte_id - 1) + quantitat); // Suma la quantitat al carret (recordem que l'ID del producte PEL USUARI és 1-based i l'índex de l'array és 0-based, per aixo fem producte_id - 1)
 
-        System.out.printf("S'han afegit %dx %s al carret!\n", quantitat, nomsProductes[producte_id - 1]);
+        System.out.printf("S'han afegit %dx %s al carret!\n", quantitat, nomsCarret.get(producte_id - 1));
         // Print de confirmació de la quantitat (int amb 'x' com suffix) i producte (string).
     }
 
@@ -92,11 +115,11 @@ public class Supermercat {
         System.out.println("--- EL TEU CARRET ---");
         double subtotal = calcularTotalMethod();
 
-        for (int i = 0; i < quantitatsCarret.length; i++) {
+        for (int i = 0; i < quantitatsCarret.size(); i++) {
             // per cada producte, si la quantitat és > 0, calcula el total del producte i suma al subtotal
-            if (quantitatsCarret[i] > 0) {
+            if (quantitatsCarret.get(i) > 0) {
 
-                System.out.printf(" · %s x%d | %.2f EUR\n", nomsProductes[i], quantitatsCarret[i], quantitatsCarret[i] * preusProductes[i]);
+                System.out.printf(" · %s x%d | %.2f EUR\n", nomsCarret.get(i), quantitatsCarret.get(i), quantitatsCarret.get(i) * preusCarret.get(i));
                 // Print del producte (string), quantitat (int amb x com prefix) i total del producte amb 2 decimals
             }
         }
@@ -113,7 +136,7 @@ public class Supermercat {
         int producte_id = -1; // guardem l'ID del producte que vol eliminar del carret
         int quantitat = -1; // guardem quants productes vol eliminar del carret
 
-        while (!(producte_id >= 1 && producte_id <= nomsProductes.length)) {
+        while (!(producte_id >= 1 && producte_id <= nomsCarret.size())) {
             System.out.print("Introdueix l'ID del producte: ");
             producte_id = input.nextInt();
         }
@@ -123,11 +146,11 @@ public class Supermercat {
             quantitat = input.nextInt();
         }
 
-        if (quantitatsCarret[producte_id - 1] >= quantitat) {
+        if (quantitatsCarret.get(producte_id - 1) >= quantitat) {
             // ens assegurem que existeixen suficients unitats del producte al carret per eliminar la quantitat demanada
 
-            quantitatsCarret[producte_id - 1] -= quantitat;
-            System.out.printf("! S'han eliminat %dx %s del carret!\n", quantitat, nomsProductes[producte_id - 1]);
+            quantitatsCarret.set(producte_id - 1, quantitatsCarret.get(producte_id - 1) - quantitat);
+            System.out.printf("! S'han eliminat %dx %s del carret!\n", quantitat, nomsCarret.get(producte_id - 1));
         } else {
             System.out.println("No hi ha suficients unitats d'aquest producte al carret!");
         }
@@ -179,33 +202,11 @@ public class Supermercat {
     public static double calcularTotalMethod() {
         double subtotal = 0.0;
 
-        for (int i = 0; i < quantitatsCarret.length; i++) {
-            if (quantitatsCarret[i] > 0) {
-                subtotal += quantitatsCarret[i] * preusProductes[i];
+        for (int i = 0; i < quantitatsCarret.size(); i++) {
+            if (quantitatsCarret.get(i) > 0) {
+                subtotal += quantitatsCarret.get(i) * preusCarret.get(i);
             }
         }
         return subtotal;
     }
 }
-
-
-/*
-=== SUPERMERCAT JAVA ===
-1. Veure catàleg
-2. Afegir producte al carret
-3. Veure carret
-4. Eliminar producte del carret
-5. Calcular total
-6. Aplicar descompte
-7. Pagar i sortir
-Tria opció: 2
-Introdueix l'ID del producte: 2
-Quantitat: 2
-S'han afegit 2x Pollastre (1kg) al carret!
-Tria opció: 3
---- EL TEU CARRET ---
-Pollastre (1kg) x2 11.98 EUR
-------------------
-Subtotal: 11.98 EUR
-
-*/
