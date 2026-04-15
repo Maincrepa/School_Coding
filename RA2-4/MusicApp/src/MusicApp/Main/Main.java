e6e6919fddf2c1a91536730878b2c3431f68fde1
Maincrepa <maincrep2020@gmail.com>
2026-04-15 11:35:00 +0200

    Progrés | + Playlist & Menu | Més contingut en Main pel Menu i Playlist

--- RA2-4/MusicApp/src/MusicApp/Main/Main.java
+++ RA2-4/MusicApp/src/MusicApp/Main/Main.java
@@ -1,10 +1,26 @@
 package MusicApp.Main;
+
 import MusicApp.Controller.Contingut;
 import MusicApp.Controller.TipusContingut.*;
+import MusicApp.View.Menu;
 
 public class Main {
     
     public static void main(String[] args) {
+        /*
+        ==========================================
+            INSTANCIES
+        ==========================================
+        */
+        //Crea una instancia de Playlist
+        //TODO
+
+        /*
+        ==========================================
+            CONTINGUT DE PROVA
+        ==========================================
+        */
+
         Contingut c = new Canco("TestingSong", "nullo", 129, "Tezting", "Pop");
         c.reproduir();
         System.out.println(c.getDescripcio());
@@ -16,5 +32,14 @@ public class Main {
         Contingut a = new Audiollibre("Audi llibrr", "Mariajose", 300, 4, "Ficció", "Español", "pepito");
         a.reproduir();
         System.out.println(a.getDescripcio());
+
+        /*
+        ==========================================
+            INSTANCIA MENU
+        ==========================================
+        */
+
+        Menu menu = new Menu();
+        menu.mostrarMenuPrincipal(); // playlist aqui
     }
 }
