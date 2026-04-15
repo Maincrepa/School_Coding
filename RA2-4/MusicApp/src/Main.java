f70da19eea11f5af4f0237c8f07a5ddbe116ce42
Maincrepa <maincrep2020@gmail.com>
2026-04-15 09:49:23 +0200

    Progress

--- RA2-4/MusicApp/src/Main.java
+++ RA2-4/MusicApp/src/Main.java
@@ -1,3 +1,9 @@
+import TipusContingut.Canco;
+
 public class Main {
     
+    public static void main(String[] args) {
+        Contingut c = new Canco("TestingSong", "nullo", 120, "Tezting", "Pop");
+        c.reproduir();
+    }
 }
--- RA2-4/MusicApp/src/TipusContingut/Canco.java
+++ RA2-4/MusicApp/src/TipusContingut/Canco.java
@@ -1,5 +1,65 @@
 package TipusContingut;
 
-public class Canco {
-    // extend
+public class Canco extends Contingut {
+    protected String album;
+    protected String genere;
+    
+    //-------------------------------------
+
+    // Constructor
+    public Canco(String titol, String autor, double duration, String album, String genere) {
+        super(titol, autor, duration);
+
+        this.album = album;
+        this.genere = genere;
+    }
+
+    // Getters
+
+    public String getAlbum() {
+        return this.album;
+    }
+
+    public String getGenere() {
+        return this.genere;
+    }
+
+    //-------------------------------------
+
+    @Override
+    public void reproduir() {
+        String output = """
+        ================================
+
+            · Ara reproduïnt:
+            %s
+            
+            - Autor: %s
+            - Duració: %s
+            - Album: %s
+            - Genere Principal: %s
+
+        ================================
+        """.formatted(
+            this.titol, 
+            this.autor, 
+            formatDuracio(this.duration),
+            this.album,
+            this.genere
+        );
+
+        System.out.println(output);
+    }
+
+
+    //-------------------------------------
+    // Metode public per transformar la duració de double en un string ben formatejat
+
+    public String getDuracioFormatejada(double duration) {
+        int mins = (int)(duration / 60); // minuts
+        int secs = (int)(duration % 60); // segons
+
+        // formateem "mm:ss"
+        return String.format("%02d:%02d", mins, secs);
+    }
 }
