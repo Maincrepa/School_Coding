7ded78eb0c3242ece5782731292d2d03b4d1eae7
Maincrepa <maincrep2020@gmail.com>
2026-05-18 11:49:57 +0200

    Implemented: getAllAnimals(), findById(id), save(animal), delete(id)

--- SpringBoot/projecte-final/adoption/src/main/java/com/example/adoption/services/AdoptionService.java
+++ SpringBoot/projecte-final/adoption/src/main/java/com/example/adoption/services/AdoptionService.java
@@ -1,24 +1,39 @@
 package com.example.adoption.services;
 
-
-
 // Package
 import com.example.adoption.model.Animal;
+import com.example.adoption.repository.AnimalRepository;
 
 // Framework
 import org.springframework.stereotype.Service;
 
 // Java
+import java.util.List;
+import java.util.Optional;
 
 // Contains business logic, called by controller
 
 @Service
 public class AdoptionService {
-    //TODO: getAllAnimals, addAnimal, updateAnimal, deleteAnimal
+    private final AnimalRepository animalRepository;
 
-    /*
-    public boolean canAdopt(Animal animal, User user) {
-        // validation logic
+    AdoptionService(AnimalRepository animalRepository) {
+        this.animalRepository = animalRepository;
     }
-    */
+
+    public List<Animal> getAllAnimals() {
+        return animalRepository.findAll();
+    }
+
+    public Optional<Animal> findById(Long id) {
+        return animalRepository.findById(id);
+    }
+
+    public Animal save(Animal animal) {
+        return animalRepository.save(animal);
+    }
+
+    public void delete(Long id) {
+        animalRepository.deleteById(id);
+    }
 }
